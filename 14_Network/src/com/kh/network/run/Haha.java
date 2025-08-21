a50d6bad889bc7f43384e5f86f13bda855a633cd
seung-2001 <ssj01022734558@gmail.com>
2025-08-21 11:46:56 +0900

    practice

--- 14_Network/src/com/kh/network/run/Haha.java
+++ 14_Network/src/com/kh/network/run/Haha.java
@@ -7,5 +7,12 @@ public class Haha {
 		System.out.println("하하하하");
 		
 	}
-
+	
+	public void practice() {
+		System.out.println("git add .");
+		System.out.println("git commit -m 커밋할이름");
+		System.out.println("git push origin master");
+		System.out.println("땡기올라면 git pull origin master");
+		
+	}
 }
