8db1626171084f138d84d07a15c8e37a1786e7bc
seung-2001 <ssj01022734558@gmail.com>
2025-08-21 17:28:38 +0900

    selectBoardList

--- 13_MVC_project/src/com/kh/idol/controller/IdolController.java
+++ 13_MVC_project/src/com/kh/idol/controller/IdolController.java
@@ -342,6 +342,13 @@ public class IdolController {
 		// 2. 요청처리
 		boards.add(board);
 	}
+	
+		
+	public List<Board> selectBoardList() {
+		// View의 요청에 의해 호출되며,
+		// 게시글 목록이 담겨있는 것을 되돌려주어야함
+		return boards;
+	}
 
 
 }
